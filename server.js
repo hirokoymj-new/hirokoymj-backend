d21327374169f01eb5b00fd5baae2d683748422a
Hiroko Yamaji <hirokoyamaji@Hirokos-MBP-2.attlocal.net>
2020-10-03 10:57:10 -0700

    schema is a separate file

--- server.js
+++ server.js
@@ -1,8 +1,9 @@
 const express = require("express");
-const { ApolloServer, gql } = require("apollo-server-express");
+const { ApolloServer } = require("apollo-server-express");
 const cors = require("cors");
 const dotEnv = require("dotenv");
 const resolvers = require("./resolvers");
+const typeDefs = require("./schema");
 
 // set env variables
 dotEnv.config();
@@ -14,41 +15,6 @@ app.use(cors());
 
 // body parser middleware
 app.use(express.json());
-
-const typeDefs = gql`
-  type Query {
-    greetings: [String]!
-    tasks: [Task!]
-    task(id: ID!): Task!
-    users: [User!]
-    user(id: ID!): User!
-  }
-
-  input createTaskInput {
-    name: String!
-    completed: Boolean!
-    userId: ID!
-  }
-
-  type Mutation {
-    createTask(input: createTaskInput): Task!
-  }
-
-  type User {
-    id: ID!
-    name: String!
-    email: String!
-    tasks: [Task!]
-  }
-
-  type Task {
-    id: ID!
-    name: String!
-    completed: Boolean!
-    #user: User!
-    user: [User!]
-  }
-`;
 
 const apolloServer = new ApolloServer({
   typeDefs,
