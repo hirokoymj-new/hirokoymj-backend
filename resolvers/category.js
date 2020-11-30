fb98de873720fb88847ef139e7f8e815b60b1bb1
Hiroko Yamaji <hirokoyamaji@Hirokos-MacBook-Pro-2.local>
2020-11-30 10:35:17 +0900

    implement subCategory type, mutation and query

--- resolvers/category.js
+++ resolvers/category.js
@@ -3,12 +3,24 @@ const jwt = require("jsonwebtoken");
 const { combineResolvers } = require("graphql-resolvers");
 
 const Category = require("../database/models/category");
+const SubCategory = require("../database/models/subCategory");
 const { isAuthenticated } = require("./middleware");
 
 module.exports = {
   Query: {
     categories: (_) => {
       return Category.find();
+    },
+    category: (_, { id }) => {
+      const category = Category.findById(id);
+      return category;
+    },
+    subCategories: (_) => {
+      return SubCategory.find();
+    },
+    subCategory: (_, { id }) => {
+      const subCategory = SubCategory.findById(id);
+      return subCategory;
     },
   },
   Mutation: {
@@ -20,6 +32,15 @@ module.exports = {
       } catch (error) {
         console.log(error);
         throw error;
+      }
+    },
+    createSubCategory: async (_, { input }) => {
+      try {
+        const subCategory = new SubCategory({ ...input });
+        const result = await subCategory.save();
+        return result;
+      } catch (error) {
+        console.log(error);
       }
     },
   },
--- typeDefs/category.js
+++ typeDefs/category.js
@@ -3,16 +3,19 @@ const { gql } = require("apollo-server-express");
 module.exports = gql`
   extend type Query {
     categories: [Category!]
+    category(id: ID!): Category!
+    subCategories: [SubCategory!]
+    subCategory(id: ID!): SubCategory
   }
 
   extend type Mutation {
     createCategory(input: createCategoryInput): Category
+    createSubCategory(input: createSubCategoryInput): SubCategory
   }
 
   type Category {
     id: ID!
     name: String!
-    # subCategories: [SubCategory!]
     createdAt: Date!
     updatedAt: Date!
   }
@@ -21,33 +24,14 @@ module.exports = gql`
     name: String!
   }
 
-  # type Topic{
-  # 	id: ID
-  # 	title: String!
-  # 	url: String!
-  # 	categoryId: ID
-  # 	subCategoryId: ID
-  # }
+  input createSubCategoryInput {
+    name: String!
+    # category: Category!
+  }
 
-  # type SubCategory {
-  #   id: ID!
-  #   name: String!
-  #   category: Category!
-  # }
+  type SubCategory {
+    id: ID!
+    name: String!
+    # category: Category!
+  }
 `;
-
-//shopping, false, hiroko
-
-// Category
-// JavaScript
-
-// Category - SubCatewgory - Topic
-// JavaScript - Basic - www.test1.com
-// JavaScript - Basic - www.test2.com
-// JavaScript - Basic - www.test3.com
-
-// JavaScript - ES6 - www.es6.com
-// JavaScript - ES6 - www.es7.com
-// JavaScript - ES6 - www.es8.com
-
-// JavaScript - Advanced JavaScript
